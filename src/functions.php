edbdf68a151319aaf3843984e6929e09a31310df
akira9889 <aki_badmin89@icloud.com>
2022-09-12 14:43:34 +0900

    各種入力項目の入力保持

---
+++ src/functions.php
@@ -0,0 +1,19 @@
+<?php
+function arrayToSelect($inputName, $srcArray, $selectedIndex = "")
+{
+    $temphtml = "<select class=\"form-select\" name=\"{$inputName}\">" . PHP_EOL;
+
+    foreach ($srcArray as $key => $val) {
+        if ($key == $selectedIndex) {
+            $selectedText = "selected";
+        } else {
+            $selectedText = "";
+        }
+        $temphtml .= "<option value=\"{$key}\"{$selectedText}>{$val}</option>" . PHP_EOL;
+    }
+
+    $temphtml .= "</select>";
+
+    return $temphtml;
+
+}
