d5127cf991ecb49f5e70a8fbfe2cdbd6fc89b593
akira9889 <aki_badmin89@icloud.com>
2022-09-26 12:37:23 +0900

    日付プルダウンにajax追加（予約上限人数に満たしている時間を非表示）

--- src/index.php
+++ src/index.php
@@ -4,6 +4,15 @@ require_once 'functions.php';
 
 $pdo = connect_db();
 
+
+$reserve_date = date('Y-m-d');
+$reserve_num = '';
+$reserve_time = '';
+$name = '';
+$email = '';
+$tel = '';
+$comment = '';
+
 //予約設定情報を取得
 $sql = "SELECT * FROM shop";
 $stmt = $pdo->query($sql);
@@ -24,8 +33,9 @@ for ($i = date('Y-m-d'); $i <= date('Y-m-d', strtotime("+{$reservable_date} day"
 
 $reserve_date_array = array_combine($reserve_date_array, $reserve_date_array);
 
-foreach ($reserve_date_array as $key => $reserve_date) {
-    $reserve_date_array[$key] = time_format_dw($reserve_date);
+//日付形式変換
+foreach ($reserve_date_array as $key => $date) {
+    $reserve_date_array[$key] = time_format_dw($date);
 }
 
 //予約最大人数選択肢配列作成
@@ -36,7 +46,7 @@ for ($i = 1; $i <= $max_reserve_num; $i++) {
 
 $reserve_num_array = array_combine($reserve_num_array, $reserve_num_array);
 
-//予約時間選択肢配列
+//予約時間選択肢配列作成
 $reserve_time_array = [];
 
 if (date('Y-m-d H:i:s', strtotime($start_time)) <= date('Y-m-d H:i:s', strtotime($end_time))) {
@@ -61,11 +71,20 @@ if (date('Y-m-d H:i:s', strtotime($start_time)) <= date('Y-m-d H:i:s', strtotime
         $i <= date('Y-m-d H:i:s', strtotime('23:00:00'));
         $i = date('Y-m-d H:i:s', strtotime($i . '+1 hours'))
     ) {
-        $reserve_time_array[] = date('G:i', strtotime($i));
+        $reserve_time_array[] = date('H:i', strtotime($i));
     }
 }
 
 $reserve_time_array = array_combine($reserve_time_array, $reserve_time_array);
+
+$reserve_list = getTotalReserveNumOfDate($reserve_date, $pdo);
+
+foreach ($reserve_list as $date_time => $total_reserve_num) {
+        if ($max_reserve_num - $total_reserve_num <= 0) {
+            $key = date('H:i', strtotime($date_time));
+            unset($reserve_time_array[$key]);
+        }
+    }
 
 session_start();
 
@@ -74,8 +93,9 @@ $err = [];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //POSTパラメータから各種入力値を受け取る
     $reserve_date = $_POST['reserve_date'];
-    $reserve_num = $_POST['reserve_num'];
     $reserve_time = $_POST['reserve_time'];
+    $reserve_date_time = $reserve_date . ' '. $reserve_time;
+    $reserve_num = $_POST['reserve_num'];
     $name = $_POST['name'];
     $email = $_POST['email'];
     $tel = $_POST['tel'];
@@ -151,15 +171,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $reserve = $stmt->fetchAll();
 
     //予約できる人数
-    $max_reservable_num = $max_reserve_num - count($reserve);
+    $reserved_num = array_sum(array_column($reserve, 'reserve_num'));
+    $max_reservable_num = $max_reserve_num - $reserved_num;
 
-    //対象の日時の人数が上限であればエラー
-    if ($max_reservable_num < $reserve_num) {
-        $err['reserve'] = time_format_dw($reserve_date) . '&nbsp&nbsp' . "{$reserve_time}時での予約人数が{$reserve_num}人だと上限に達してしまうため、{$max_reservable_num}人までしか予約できません。もしくは違う時間を選んでください。";
-    }
-    if ($max_reservable_num == 0) {
-        $err['reserve'] = 'この時間での予約は埋まりました。違う時間を選んでください。';
-    }
 
     $stmt = null;
     $pdo = null;
@@ -168,16 +182,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($err)) {
         //各種入力値をセッション変数に保存する
         $_SESSION['RESERVE']['reserve_date'] = $reserve_date;
-        $_SESSION['RESERVE']['reserve_num'] = $reserve_num;
         $_SESSION['RESERVE']['reserve_time'] = $reserve_time;
+        $_SESSION['RESERVE']['reserve_date_time'] = $reserve_date_time;
+        $_SESSION['RESERVE']['reserve_num'] = $reserve_num;
         $_SESSION['RESERVE']['name'] = $name;
         $_SESSION['RESERVE']['email'] = $email;
         $_SESSION['RESERVE']['tel'] = $tel;
         $_SESSION['RESERVE']['comment'] = $comment;
 
-        // 予約確認画面へ遷移
-        header('Location: confirm.php');
-        exit;
+        //対象の日時の人数が上限であればエラー
+        if ($max_reservable_num < $reserve_num) {
+            $err['reserve'] = time_format_dw($reserve_date) . '&nbsp&nbsp' . "{$reserve_time}時での予約人数が{$reserve_num}人だと上限に達してしまうため、{$max_reservable_num}人までしか予約できません。もしくは違う時間を選んでください。";
+        }
+
+        if ($max_reservable_num == 0) {
+            $err['reserve'] = 'この時間での予約は埋まりました。違う時間を選んでください。';
+        }
+
+        if (empty($err)) {
+            // 予約確認画面へ遷移
+            header('Location: confirm.php');
+            exit;
+        }
     }
 } else {
     //セッションに入力情報がある場合は取得する
@@ -189,15 +215,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = $_SESSION['RESERVE']['email'];
         $tel = $_SESSION['RESERVE']['tel'];
         $comment = $_SESSION['RESERVE']['comment'];
-    } else {
-        //存在しない場合は各変数を初期化
-        $reserve_date = '';
-        $reserve_num = '';
-        $reserve_time = '';
-        $name = '';
-        $email = '';
-        $tel = '';
-        $comment = '';
     }
 }
 $path = '';
@@ -214,17 +231,17 @@ $page_title = 'ご来店予約';
         <p style="color: #dc3545;"><?php if (isset($err['reserve'])) echo $err['reserve'] ?></p>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">【1】予約日を選択</label>
-            <?= arrayToSelect('reserve_date', $reserve_date_array, $err['reserve_date'], $reserve_date) ?>
+            <?= arrayToSelect('reserve_date', $reserve_date_array, [$err['reserve_date']], $reserve_date) ?>
             <div class="invalid-feedback"><?= $err['reserve_date'] ?></div>
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">【2】人数選択</label>
-            <?= arrayToSelect('reserve_num', $reserve_num_array, $err['reserve_num'], $reserve_num) ?>
+            <?= arrayToSelect('reserve_num', $reserve_num_array, [$err['reserve_num'],$err['reserve']], $reserve_num) ?>
             <div class="invalid-feedback"><?= $err['reserve_num'] ?></div>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">【3】予約時間を選択</label>
-            <?= arrayToSelect('reserve_time', $reserve_time_array, $err['reserve_time'], $reserve_time) ?>
+            <label for="exampleFormControlInput1" class="form-label">【3】予約時間を選択  <?= '営業時間(' . substr($start_time, 0, -3).' 〜 '.substr($end_time, 0, -3).') ' ?><?php if($start_time > $end_time) echo '＊夜中の時間帯で予約する方は日付にご注意ください。' ?></label>
+            <?= arrayToSelect('reserve_time', $reserve_time_array, [$err['reserve_time']], $reserve_time) ?>
             <div class="invalid-feedback"><?= $err['reserve_time'] ?></div>
         </div>
         <div class="mb-3">
@@ -250,11 +267,23 @@ $page_title = 'ご来店予約';
             <button class="btn btn-primary rounded-pill" type="submit">確認画面へ</button>
         </div>
     </form>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <?php include('templates/footer.php'); ?>
+    <script>
+        $('#reserve_date').on('change', function() {
+            $.ajax({
+                url: 'ajax_select_change.php', //データベースを繋げるファイル
+                type: "POST",
+                data: {
+                    date: $(this).val(), //選択されたデータ取得
+                }
+            }).done(function(html) {
+                $("#reserve_time").empty();
+                $("#reserve_time").append(html);
+            }).fail(function(html) {
+                alert("error"); //通信失敗時
+            });
+        });
+    </script>
 </body>
 
 </html>
